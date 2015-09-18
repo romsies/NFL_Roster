0499caf5c3b2df5abb820bdcd233b89e151d2a05
James Stoddard <romsies.js@gmail.com>
2015-09-17 19:19:39 -0600

    created nfl roster, added new player button

---
+++ app.js
@@ -0,0 +1,36 @@
+
+function addPlayer(e) {
+      e.preventDefault();
+      var name = $('#name').val();
+      var position = $('#position').val();
+      var num = $('#num').val();
+      
+      var html = '<div class="player-card">' +
+      '<div class="">' +
+      '<div class="player-image"> <img src="player.png" alt="Player 1"></div>' +
+      '<div>' +
+      '<span>Player Name: </span>'+name +
+      '</div>' +
+      '<div>' +
+      '<span>Position: </span>'+ position +
+      '</div>' +
+      '<div>' +
+      '<span>Number: </span>'+num +
+      '</div>' +
+      '</div>' +
+      '<button class="btn btn-danger remove-player">REMOVE</button></div>'
+      
+
+      
+ $(".player-roster").append(html);     
+}
+
+$('#add-player-button').on('click', addPlayer);
+
+$('.player-roster').on('click', '.remove-player', removePlayer);
+
+
+function removePlayer(){
+      var removeBtn = $(this);
+      removeBtn.parent('.player-card').remove();
+}
